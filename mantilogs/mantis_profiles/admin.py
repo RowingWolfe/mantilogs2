41a80b4cadbe80c370c5229db66eeb8089b0987c
RhodiumRocket <rowingwolfe@gmail.com>
2020-07-04 11:36:01 -0400

    Update admin.py
    This is what you get when you dev on live. Forgot to update the admin interface registration.

--- mantilogs/mantis_profiles/admin.py
+++ mantilogs/mantis_profiles/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Mantis, Logs, Picture, Environment_Log, Feeder_Culture, Feeder_Log, Gecko, Gecko_Log, Gecko_Morph
+from .models import Mantis, Logs, Picture, Environment_Log, Culture, Culture_Log, Gecko, Gecko_Log, Gecko_Morph
 # Register your models here.
 
 admin.site.register(Mantis)
 admin.site.register(Logs)
 admin.site.register(Picture)
 admin.site.register(Environment_Log)
-admin.site.register(Feeder_Culture)
-admin.site.register(Feeder_Log)
+admin.site.register(Culture)
+admin.site.register(Culture_Log)
 admin.site.register(Gecko)
 admin.site.register(Gecko_Log)
 admin.site.register(Gecko_Morph)
