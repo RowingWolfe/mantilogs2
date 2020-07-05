1510890b07d613e717327e557daea5ee5662badc
RhodiumRocket <rowingwolfe@gmail.com>
2020-07-05 13:46:10 -0400

    Update admin.py

--- mantilogs/mantis_profiles/admin.py
+++ mantilogs/mantis_profiles/admin.py
@@ -4,9 +4,10 @@ from .models import Mantis, Logs, Picture, Environment_Log, Culture, Culture_Log
 
 class LogsAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
+    admin.site.register(Logs)
 
 admin.site.register(Mantis)
-admin.site.register(Logs)
+
 admin.site.register(Picture)
 admin.site.register(Environment_Log)
 admin.site.register(Culture)
