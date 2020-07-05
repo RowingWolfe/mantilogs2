adf44a642af5127cf90890c60d2b6bc6b5fdc9a8
RhodiumRocket <rowingwolfe@gmail.com>
2020-07-05 13:25:38 -0400

    Update admin.py
    Attempt to change date heirarchy to make logs show by log date rather than update date.

--- mantilogs/mantis_profiles/admin.py
+++ mantilogs/mantis_profiles/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Mantis, Logs, Picture, Environment_Log, Culture, Culture_Log, Gecko, Gecko_Log, Gecko_Morph
 # Register your models here.
 
+class LogsAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+
 admin.site.register(Mantis)
 admin.site.register(Logs)
 admin.site.register(Picture)
