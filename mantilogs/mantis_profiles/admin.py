de15470595cc3be1cb2e83ca106561ff76e765db
Roanoke <rowingwolfe@gmail.com>
2020-10-06 12:56:49 -0400

    Cleaned up a little. Added inventory to admin system.

--- mantilogs/mantis_profiles/admin.py
+++ mantilogs/mantis_profiles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Mantis, Logs, Picture, Environment_Log, Culture, Culture_Log, Gecko, Gecko_Log, Gecko_Morph, Mantis_Ooth, Gecko_Egg_Clutch
+from .models import Mantis, Logs, Picture, Environment_Log, Culture, Culture_Log, Gecko, Gecko_Log, Gecko_Morph, \
+    Mantis_Ooth, Gecko_Egg_Clutch, Inventory_Item, Inventory_Consumption, Purchase
 # Register your models here.
 
 class LogsAdmin(admin.ModelAdmin):
@@ -16,3 +17,6 @@ admin.site.register(Gecko_Log)
 admin.site.register(Gecko_Morph)
 admin.site.register(Mantis_Ooth)
 admin.site.register(Gecko_Egg_Clutch)
+admin.site.register(Inventory_Consumption)
+admin.site.register(Inventory_Item)
+admin.site.register(Purchase)
