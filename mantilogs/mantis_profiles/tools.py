df82d3d77ca478833996e1855ce7cbc99a39e580
Roanoke <rowingwolfe@gmail.com>
2020-10-13 10:00:44 -0400

    Fixed filter refactor, hadn't had my tea yet..

--- mantilogs/mantis_profiles/tools.py
+++ mantilogs/mantis_profiles/tools.py
@@ -4,17 +4,17 @@
 
 from .models import Gecko_Log, Gecko
 
-#Originally I was going to write these all
+#Originally I was going to write these all as one single func but that would run slow so here we are.
 def get_last_tank_clean(gecko):
     """Return the last time the tank was full cleaned for <gecko>"""
     if Gecko_Log.objects.filter(gecko=gecko.name).filter(habitat_full_clean=True):
-        return objects.filter(gecko=gecko.name).order_by('date').filter(habitat_full_clean=True).latest('date').date
+        return Gecko_Log.objects.filter(gecko=gecko.name).order_by('date').filter(habitat_full_clean=True).latest('date').date
 
 
 def get_last_vitd(gecko):
     """Return the last time <gecko> was given Vitamin D supplement."""
     if Gecko_Log.objects.filter(gecko=gecko.name).filter(calc_with_vit_d=True):
-        return objects.filter(gecko=gecko.name).order_by('date').filter(calc_with_vit_d=True).latest('date').date
+        return Gecko_Log.objects.filter(gecko=gecko.name).order_by('date').filter(calc_with_vit_d=True).latest('date').date
 
 
 def get_last_multivit(gecko):
