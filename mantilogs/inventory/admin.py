d11d221cfcef0a68b786c9eb23babb0bc89850dc
Roanoke <rowingwolfe@gmail.com>
2020-10-27 08:36:02 -0400

    Added inv to admin

--- mantilogs/inventory/admin.py
+++ mantilogs/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item, Inventory, Item_Consumption, Order
 
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(Inventory)
+admin.site.register(Item_Consumption)
+admin.site.register(Order)
